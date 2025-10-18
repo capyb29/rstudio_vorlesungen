4f18ada0644b81effb9cc16f6437eb7f21dfe08d
Philipp <100852906+capyb29@users.noreply.github.com>
2025-10-18 12:15:17 +0200

    Add files via upload

---
+++ main.R
@@ -0,0 +1,19 @@
+seq1 = as.numeric(1:10)
+rep1 <- as.numeric(rep(1:3,times=3,length.out = 10))
+rep2 <- as.numeric(rep(1:5,each=2))
+rep3 <- as.numeric(rep(1:3,each=2,length.out = 10))
+
+#Aufgabe 2.1
+l2_1 <- 5*3+2^4
+#Aufgabe 2.2
+l2_2 <- 7/4*sqrt(16)
+#Aufgabe 2.3
+ex2_3 <- 36^(1/3) 
+l2_3 <- round(ex2_3,digits = 2)
+#Aufgabe 2.4
+ex2_4 <- (((2+2)^3)+(18-7)^2)/(8^3/3)
+l2_4 <- round(ex2_4,digits = 2)
+#Aufgabe 2.5
+l2_5 <- sum(1:10)
+#Aufgabe 2.6
+
