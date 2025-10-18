28325d6bd9200deb19bb272986d2425ec75fd00b
Maximilian Priesack <maxi@Maximilians-Laptop.local>
2025-10-18 13:14:28 +0200

    fuck git

--- uebungsaufgaben.R
+++ uebungsaufgaben.R
@@ -1,17 +1,25 @@
-absatzmenge <- c(10, 12, 8, 9, 11, 19, 9)
-verkaufstage <- c("Mo","Di","Mi","Do","Fr","Sa","So")
-names(absatzmenge) <- verkaufstage
+Absatzmenge <- c(10, 12, 8, 9, 11, 19, 9)
+Verkaufstage <- c("Mo","Di","Mi","Do","Fr","Sa","So")
+names(Absatzmenge) <- Verkaufstage
 
-repiti <- rep(1:5)
-sequi <- seq(1:5) 
+# Übungsaufgabe 2
+repitition <- rep(1:5)
+sequiti <- seq(1:5)
 
-mat <- as.numeric(rep(1:5))
-mat2 <- as.numeric(rep(6:10))
+# Übungsaufgabe 3
+m_v1 <- as.numeric(rep(1:5))
+m_v2 <- as.numeric(rep(6:10))
 
-joined <- append(mat,mat2)
-DS.MATRIX <- matrix(joined,nrow = 2,ncol = 5, byrow = TRUE)
+joined_m_v1_v2 <- append(m_v1, m_v2)
 
-zeilensumme <- c(sum(DS.MATRIX[1,]),sum(DS.MATRIX[2,]))
-
-DS.MATRIX2 <- matrix(zeilensumme)
+DS.MATRIX <- matrix(joined_m_v1_v2, nrow = 2, ncol = 5, byrow = TRUE)
+# Aufgabe 4
+summe <- c(sum(DS.MATRIX[1,]),sum(DS.MATRIX[2,]))
+DS.MATRIX2 <- matrix(summe)
 DS.MATRIX3 <- cbind(DS.MATRIX,DS.MATRIX2)
+
+spalten <- c("S1", "S2", "S3", "S4", "S5", "S6")
+zeilen <- c("Z1", "Z2")
+
+colnames(DS.MATRIX3) <- spalten
+rownames(DS.MATRIX3) <- zeilen
