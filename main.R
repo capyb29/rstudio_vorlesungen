fa84de8f85e57ff571b0a14635b26388a2b47062
Philipp Weingart <w2005@gmx.de>
2025-10-20 23:41:58 +0200

    fertig mit aufgabensammlung 1

--- main.R
+++ main.R
@@ -16,5 +16,24 @@ l2_4 <- round(ex2_4,digits = 2)
 #Aufgabe 2.5
 l2_5 <- sum(1:10)
 #Aufgabe 2.6
-
+ex2_6 <- 6:32
+l2_6 <- sum(2*ex2_6+1)
+#Aufgabe 2.7
+ex2_7 <- 2:9
+l2_7 <- 2*sum(ex2_7/4)
+#Aufgabe 2.8
+ex2_8 <- 1:10
+l2_8 <- sum(2^ex2_8)
+#Aufgabe 2.9
+ex2_9 <- 1:20
+l2_9 <- sum(2*ex2_9)
+#Aufgabe 2.10
+ex2_10 <- 1:10
+l2_10 <- 2*2^(ex2_10-1)
+#Aufgabe 2.11
+ex2_11 <- 1:5
+l2_11 <- prod(2*2^(ex2_11-1))
+#Aufgabe 2.12
+ex2_12 <- 1:10
+l2_12 <- (ex2_12^2)*3
 # Hallo ich teste Git
