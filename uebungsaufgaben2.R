faf98f0034045ef943858be2d5966c5e05ecd48b
Philipp Weingart <w2005@gmx.de>
2025-10-25 12:37:20 +0200

    uebungsaufgaben für tag 2 25.10

--- uebungsaufgaben2.R
+++ uebungsaufgaben2.R
@@ -9,3 +9,17 @@ df.nameOfCollumns <- colnames(df)
 df.nameOfRows <- rownames(df)
 df.dimensions <- dim(df)
 
+df$s3 = rep(NA,nrow(df))
+df$s4 = df$s1 * 2
+df$s5 = df$s4 *2
+df$s6 = df$s1 / 100
+
+dfneu = data.frame (s1=100,s2 = "z", s3=NA,s4=500,s5=3000,s6=1)
+df = rbind(df,dfneu)
+
+quarti = c(P1=1,P2=3,P3=4,P4=6,P5=8,P6=10,P7=12,P8=13,P9=15,P10=19,P11=30)
+quarti.mittelwert = mean(quarti)
+quarti.median = median(quarti)
+quarti.varianz = var(quarti)
+quarti.standardabweichung = sd(quarti)
+quarti.spannweite = max(quarti) - min(quarti)
