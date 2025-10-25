03c8606fce03da292b5ba507fc172537b1b66e55
Philipp Weingart <w2005@gmx.de>
2025-10-25 11:01:12 +0200

    uebungsdings2

---
+++ uebungsaufgaben2.R
@@ -0,0 +1,11 @@
+tag <- c("Mo","Di","Mi","Do","Fr")
+wert <- c(320,325,330,310,320)
+order <- c(FALSE,TRUE,FALSE,FALSE,TRUE)
+
+df <- data.frame(s1=1:10,s2=letters[1:10])
+df.numberOfCollumns <- ncol(df)
+df.numberOfRows <- nrow(df)
+df.nameOfCollumns <- colnames(df)
+df.nameOfRows <- rownames(df)
+df.dimensions <- dim(df)
+
