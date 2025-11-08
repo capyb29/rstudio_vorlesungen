7d8e874c0d66f747c1c229c6a512f6ea98c20b7c
Philipp Weingart <w2005@gmx.de>
2025-11-08 11:18:19 +0100

    ich drehe dorsch

--- main2.R
+++ main2.R
@@ -1,10 +1,5 @@
 # Dataframe daten csv
-<<<<<<< HEAD
-bev = read.csv("bev.csv", sep=";")
-bev = bev[,-2]
-=======
 bev <- read.csv("bev.csv", sep=";")
 bev = bev[,-2:-5]
 colnames(bev)[colnames(bev)=="X31.12.2019"] <- "Stichtag 31.12.2019"
 bev_na = is.na(bev)
->>>>>>> 4ad8767 (anfang übungsaufgaben3)
