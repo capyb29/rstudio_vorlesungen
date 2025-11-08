31454c81dc33b5530b8d342eca3598ce4d6e64f5
omardaus <125575247+omardaus@users.noreply.github.com>
2025-11-08 12:08:05 +0100

    aufgabe 2

--- uebungsaufgaben3.R
+++ uebungsaufgaben3.R
@@ -12,3 +12,13 @@ bestellungen = fread("bestellungen_output.csv")
 umsatz2019 = read_excel("umsatz.xlsx", sheet = "2019")
 umsatz2019$Produktpreis = umsatz2019$Umsatz / umsatz2019$Bestellungen
 writexl::write_xlsx(umsatz2019, "umsatz_2019_output.xlsx")
+
+
+# Aufgabe 2
+
+vektor = c(1:5)
+matrix1 = matrix(c(1:10), nrow=5, ncol=2)
+df = data.frame(spalte.1 = 10, spalte.2 = letters[1:10])
+
+
+list = list(Vektor = vektor, Matrix = matrix1, DataFrame = df)
