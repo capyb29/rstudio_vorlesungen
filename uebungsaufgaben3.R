5a2c3a37f9824c202c4ac9e4c6a7757831a5898a
omardaus <125575247+omardaus@users.noreply.github.com>
2025-11-08 11:30:52 +0100

    xl

--- uebungsaufgaben3.R
+++ uebungsaufgaben3.R
@@ -1,4 +1,14 @@
 
 library(data.table)
+library(readxl, writexl)
 
-bsp = fread("beispiel.csv", sep="auto")
+bsp = fread("beispiel.csv")
+bsp$mwst = '19%'
+
+write.csv(bsp, file="bestellungen_output.csv", row.names = FALSE)
+bestellungen = fread("bestellungen_output.csv")
+
+
+umsatz2019 = read_excel("umsatz.xlsx", sheet = "2019")
+umsatz2019$Produktpreis = umsatz2019$Umsatz / umsatz2019$Bestellungen
+writexl::write_xlsx(umsatz2019, "umsatz_2019_output.xlsx")
