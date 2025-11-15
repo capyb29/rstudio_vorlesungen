714a23580fb3dcb51ae93c62bcdaac11b61650ab
omardaus <125575247+omardaus@users.noreply.github.com>
2025-11-15 15:15:54 +0100

    aufgabe 2

--- main3.R
+++ main3.R
@@ -5,5 +5,16 @@ klausurergebnis = data.frame(
   Note = c(0.7, 1.3, 2, 3, 4, 3, 5, 3.7)
 )
 
-cor = cor(klausurergebnis$Stunden, klausurergebnis$Note)
+note_stunden_korr = cor(klausurergebnis$Stunden, klausurergebnis$Note)
 # stark negative Korrelation | je höher die Stunden, desto besser die Note
+
+# Aufgabe 2
+umsatz_vertieb = read.csv2("umsatz_vertrieb.csv")
+umsatz_vetriebskosten_korr = cor(umsatz_vertieb$Vertriebskosten, umsatz_vertieb$Umsatz)
+
+mx = mean(umsatz_vertieb$Vertriebskosten)
+my = mean(umsatz_vertieb$Umsatz)
+r = (sum((umsatz_vertieb$Vertriebskosten - mx)*(umsatz_vertieb$Umsatz - my))) / 
+    sqrt(sum((umsatz_vertieb$Vertriebskosten - mx)^2) * sum((umsatz_vertieb$Umsatz - my)^2))
+
+# manuelle formel und funktion gleiches ergebnis
